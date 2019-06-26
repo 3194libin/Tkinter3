4bdf2d7318db3856b54eb2824d2abc264c2ac535
3194libin <15764747737@qq.com>
2019-06-26 15:15:03 +0800

    含有图片

--- TKinter3.py
+++ TKinter3.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 root = Tk()
 
-textLable = Label(root,text = '您所下载的电影含有未成年人限制内容，\n请满18周岁后观看！')
+textLable = Label(root,text = '您所下载的电影含有未成年人限制内容，\n请满18周岁后观看！',
+                  justify=LEFT,padx = 10)
 textLable.pack(side = LEFT)
 
 photo  =  PhotoImage(file="18.gif")
