4d41158aa23c41a60f6d94b378706e7da09e7104
3194libin <15764747737@qq.com>
2019-06-26 16:11:55 +0800

    button响应

--- TKinter3.py
+++ TKinter3.py
@@ -1,14 +1,27 @@
 from tkinter import *
 
+def callback():
+    var.set("吹吧你，我才不信！")
 root = Tk()
 
+frame1 = Frame(root)
+frame2 = Frame(root)
+
+var = StringVar()
+var.set('您所下载的影片含有未成年人限制内容\n请满18周岁后观看')
+textLable = Label(frame1,
+                  textvariable=var,
+                  justify=LEFT,
+                  padx=10)
+textLable.pack(side=LEFT)
+
 photo = PhotoImage(file='18.gif')
-theLable = Label(root,
-                 text='学习python\n到哔哩哔哩',
-                 justify=LEFT,
-                 image = photo,
-                 compound=CENTER,
-                 font=("华康少女字体",20),
-                 fg = "purple")
-theLable.pack()
+imgLable = Label(frame1,image=photo)
+imgLable.pack(side = RIGHT)
+
+theBurron = Button(frame2,text='我已满18岁',command=callback)
+theBurron.pack()
+
+frame1.pack( padx=10,pady=10)
+frame2.pack()
 mainloop()
